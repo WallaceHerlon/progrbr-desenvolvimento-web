d93a5b6b45404941cf720015f45e8082997b0a69
Wallace Herlon Silva <wherlon@hotmail.com>
2022-11-09 19:32:34 -0300

    Aula JavaScript Básico - aula 2
    Objetos propriedades, métodos, construtores, Datas, Cont, Let e Var.

---
+++ 03_JavaScript/js/15_datas.js
@@ -0,0 +1,10 @@
+// Formas de instanciar uma Data:
+
+// var d = new Date();
+// var d = new Date(milliseconds);
+// var d = new Date(dateString);
+// var d = new Date(year, month, day, hours, minutes, seconds, milliseconds);
+
+var d = new Date()
+
+console.log(d)
