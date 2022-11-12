672f8c097f05ef3a0575cc5303e9d25991d2da8b
Wallace Herlon Silva <wherlon@hotmail.com>
2022-11-12 20:48:29 -0300

    Projeto 04: Churrascômetro - Logica JavaScript

--- projects/04_churrascometro(HTML_JS_básico)/js/script.js
+++ projects/04_churrascometro(HTML_JS_básico)/js/script.js
@@ -2,3 +2,47 @@
 // Cerveja - 1200 ml por pessoas + de 6 horas - 2000 ml
 // Refrigerante/agua - 1000 ml por pessoa + 6 horas 1500 ml
 // Criança vale por 0,5
+
+let inputAdultos = document.getElementById("adultos")
+let inputCriancas = document.getElementById("criancas")
+let inputDuracao = document.getElementById("duracao")
+let resultado = document.getElementById("resultado")
+
+function calcular() {
+    let adultos = inputAdultos.value;
+    let criancas = inputCriancas.value;
+    let duracao = inputDuracao.value;
+
+    let qdtTotalCarne = carnePP(duracao) * adultos + (carnePP(duracao) / 2 * criancas);
+    let qdtTotalCerveja = cervejaPP(duracao) * adultos;
+    let qdtTotalBebidas = bebidasPP(duracao) * adultos + (bebidasPP(duracao) / 2 * criancas);
+
+    resultado.innerHTML = `<p>${qdtTotalCarne / 1000} Kg de carne<p>`;
+    resultado.innerHTML += `<p>${qdtTotalCerveja}ml de Cerveja`;
+    resultado.innerHTML += `<p>${qdtTotalBebidas}ml de Bebida`;
+    
+}
+
+function carnePP(duracao) {
+    if (duracao >= 6) {
+        return 650;
+    } else {
+        return 400;
+    }
+}
+
+function cervejaPP(duracao) {
+    if (duracao >= 6) {
+        return 2000;
+    } else {
+        return 1200;
+    }
+}
+
+function bebidasPP(duracao) {
+    if (duracao >= 6) {
+        return 1500;
+    } else {
+        return 1000;
+    }
+}
