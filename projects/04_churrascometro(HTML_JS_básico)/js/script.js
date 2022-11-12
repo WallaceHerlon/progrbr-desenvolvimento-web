76659259e12a658b69cb532cde5526ee42f38a42
Wallace Herlon Silva <wherlon@hotmail.com>
2022-11-12 19:45:18 -0300

    Projeto 04: Churrascômetro  - Setup inicial

---
+++ projects/04_churrascometro(HTML_JS_básico)/js/script.js
@@ -0,0 +1,4 @@
+// Carne - 400 gr por pessoas + de 6 horas - 650 gr
+// Cerveja - 1200 ml por pessoas + de 6 horas - 2000 ml
+// Refrigerante/agua - 1000 ml por pessoa + 6 horas 1500 ml
+// Criança vale por 0,5
