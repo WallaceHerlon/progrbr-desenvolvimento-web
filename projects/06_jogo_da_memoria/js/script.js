be20505dea3fc57cbd539733f7330ae1307e9fc9
Wallace Herlon Silva <wherlon@hotmail.com>
2022-11-20 22:28:12 -0300

    Projeto 06: Jogo da memória - Montando o tabuleiro

--- projects/06_jogo_da_memoria/js/script.js
+++ projects/06_jogo_da_memoria/js/script.js
@@ -1,5 +1,7 @@
 const FRONT = "card_front";
 const BACK = "card_back";
+const CARD = "card";
+const ICON = "icon";
 
 let techs = ['bootstrap', 'css', 'electron', 'firebase', 'html', 'javascript', 'jquery', 'mongo', 'node', 'react'];
 
@@ -10,7 +12,41 @@ startGame();
 function startGame() {
     cards = createCardsFromTechs(techs);
     shuffleCards(cards);
-    console.log(cards);
+    initializeCards(cards);
+}
+
+function initializeCards(cards) {
+    let gameBoard = document.getElementById("gameBoard");
+
+    cards.forEach(card => {
+        let cardElement = document.createElement('div');
+        cardElement.id = card.id;
+        cardElement.classList.add(CARD);
+        cardElement.dataset.icon = card.icon;
+        createCardsContent(card, cardElement);
+        cardElement.addEventListener('click', flipCard);
+        gameBoard.appendChild(cardElement);
+    })
+}
+
+function createCardsContent(card, cardElement) {
+    
+    createCardFace(FRONT, card, cardElement);
+    createCardFace(BACK, card, cardElement);
+}
+
+function createCardFace(face, card, element) {
+    let cardElementFace = document.createElement('div');
+    cardElementFace.classList.add(face);
+    if(face === FRONT) {
+        let iconElement = document.createElement('img');
+        iconElement.classList.add(ICON);
+        iconElement.src = "./img/" + card.icon + ".png";
+        cardElementFace.appendChild(iconElement);
+    } else {
+        cardElementFace.innerHTML = "&lt/&gt";
+    }
+    element.appendChild(cardElementFace);
 }
 
 function shuffleCards(cards) {
@@ -53,4 +89,9 @@ function createPairFromTech(tech) {
 function createIdWithTech(tech) {
     
     return tech + parseInt(Math.random() * 1000);
+}
+
+function flipCard() {
+    
+    this.classList.add("flip");
 }
