3713cff50f12b06c1bc0e4ae127c07b5503f9f51
Wallace Herlon Silva <wherlon@hotmail.com>
2022-11-20 21:42:38 -0300

    Projeto 06: Jogo da memória - Embaralhando as cartas

--- projects/06_jogo_da_memoria/js/script.js
+++ projects/06_jogo_da_memoria/js/script.js
@@ -3,6 +3,28 @@ const BACK = "card_back";
 
 let techs = ['bootstrap', 'css', 'electron', 'firebase', 'html', 'javascript', 'jquery', 'mongo', 'node', 'react'];
 
+let cards = null;
+
+startGame();
+
+function startGame() {
+    cards = createCardsFromTechs(techs);
+    shuffleCards(cards);
+    console.log(cards);
+}
+
+function shuffleCards(cards) {
+    let currentIndex = cards.length;
+    let randomIndex = 0;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        [cards[randomIndex], cards[currentIndex]] = [cards[currentIndex], cards[randomIndex]];  
+    }
+}
+
 createCardsFromTechs(techs);
 function createCardsFromTechs(techs) {
     
