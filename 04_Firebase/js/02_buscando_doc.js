5ae91295b7b318da79c8acaf2235ce6836798546
Wallace Herlon Silva <wherlon@hotmail.com>
2022-11-21 21:14:45 -0300

    Aula Firebase - Firestore
    Introdução Firestore, Conhecendo Firebase, Criando conta, Criando projeto, Coleções documentos, Conectando Firebase Js, lendo documentos.

---
+++ 04_Firebase/js/02_buscando_doc.js
@@ -0,0 +1,21 @@
+// Initialize Firebase
+const config = {
+  apiKey: "AIzaSyCJDH92u2WtvaQ9xZk3IV43uDgImBzhBjA",
+  authDomain: "colegio-4df79.firebaseapp.com",
+  projectId: "colegio-4df79",
+  storageBucket: "colegio-4df79.appspot.com",
+  messagingSenderId: "156788156548",
+  appId: "1:156788156548:web:31681b0205a6586e6b7ad1"
+};
+
+firebase.initializeApp(config);
+
+let db = firebase.firestore();
+
+db.collection("turmaA").where("nome", "==", "José").get()
+                       .then((snapshot) => {
+                            snapshot.forEach((doc) => {
+                                let aluno = doc.data();
+                                console.log(aluno.nome, aluno.sobrenome);
+                            })
+                       })
