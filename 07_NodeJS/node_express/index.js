1362a70de232a67adea90d2da123da2a0d4f6798
Wallace Herlon Silva <wherlon@hotmail.com>
2022-12-03 21:07:45 -0300

    Aula Node.Js - Aula1
    Arquivo estáticos com express, Get, Post, Put, Delete, Rest client.

--- 07_NodeJS/node_express/index.js
+++ 07_NodeJS/node_express/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-const PORT = 5000;
+app.use(express.static(path.join(__dirname, 'client')))
 
+app.get("/", (req, res) => {
+    res.type("html");
+    res.send("<h1>Hello wornd from GET</h1>")
+})
+
+const PORT = 5000;
 app.listen(PORT, ()=>{
     console.log(`Server Running on Port:${PORT}`);
 })
