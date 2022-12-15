2bb73469559d032bbdb912459d4442df0569fd6d
Wallace Herlon Silva <wherlon@hotmail.com>
2022-12-15 18:58:12 -0300

    Aula Login JWT - Aula 3
    Criando o Token, Protegendo rotas, Validando a entrada.

--- 09_login JWT/controllers/userController.js
+++ 09_login JWT/controllers/userController.js
@@ -1,5 +1,6 @@
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken')
 
 const userController = {
     register: async function register(req, res) {
@@ -26,6 +27,10 @@ const userController = {
 
         const passwordAndUserMatch = bcrypt.compareSync(req.body.password, selectedUser.password)
         if (!passwordAndUserMatch) return res.status(400).send('Email or password incorrect')
+
+        const token = jwt.sign({ _id: selectedUser._id }, process.env.TOKEN_SECRET)
+
+        res.header('authoriztion-token', token)
 
         res.send("User Logged")
     }
