6b044914452d8b3bbf0c6ed565e41598a6f9764a
Wallace Herlon Silva <wherlon@hotmail.com>
2022-12-21 21:15:14 -0300

    Aula React - Aula1
    Build do React,  Componentes e propriedades, Listas, Criando Objetos com Class, Componentes com Class, Estados.

---
+++ 10_react/meu-app/src/Menu.js
@@ -0,0 +1,14 @@
+
+
+function Menu(props) {
+
+    const listLinks = props.links.map((link, index) => <li key={index}>{link}</li>)
+
+    return (
+        <ul className="menu">
+            {listLinks}
+        </ul>
+    )
+}
+
+export default Menu;
